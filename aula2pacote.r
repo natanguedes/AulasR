4acc9b64de57d3423d227da80109d207b2504406
ngsneto@gmail.com <silvinha1234567>
2020-03-09 20:29:09 -0300

    add

---
+++ aula2pacote.r
@@ -0,0 +1,8 @@
+install.packages("caret")#para algoritmo machine learning
+
+install.packages("randomForest")#para algoritmo machine learning, O randomForest implementa o algoritmo de floresta aleatória, grupo de algoritmo de aprendizado de máquina
+install.packages("ggplot2") #visualização de gráficos
+install.packages("dplyr")#pacote de manipulação de dados
+install.packages("devtools")#permites instalação de pacote
+
+library(ggplot2) #diz ao r que é pra usar o pacote ggplot2

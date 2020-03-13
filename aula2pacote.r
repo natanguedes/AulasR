8cd79302f8a13095952eaefea7713d67772d2d6e
ngconsultoriadedados <ngconsultoriadedados@gmail.com>
2020-03-13 09:28:46 -0300

    Update aula2pacote.r

--- aula2pacote.r
+++ aula2pacote.r
@@ -7,3 +7,19 @@ install.packages("devtools")#permites instalação de pacote
 
 library(ggplot2) #diz ao r que é pra usar o pacote ggplot2
 detach(package:ggplot2) #descarrega o pacote ou seja, o mesmo não pode ser mais utilizado nessa sessão
+
+help(mean)#ajuda da m?dia
+#busca mais informa??o sobre a fun??o
+install.packages("sos")
+
+#usa a fun??o na sess?o
+library(sos)
+library(brew)
+#como utilizar essa fun??o
+findFn("fread")
+#faz uma busca no RSite
+RSiteSearch("matplotlib")
+#mostra um exemplo da fun??o ou pacote
+example("mean")
+example("matplot")
+

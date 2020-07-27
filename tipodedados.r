7e74da75cda905048c8075024f89eab565e9457a
natanguedes <ngsneto@gmail.com>
2020-07-26 21:52:54 -0300

    add

---
+++ tipodedados.r
@@ -0,0 +1,52 @@
+setwd("E:\\Natanael\\tsi\\3periodo\\estatistica\\NovaPasta\\aulas\\AulasR")
+
+num = 7
+num
+class(num)
+mode(num)
+typeof(num)
+
+
+num2 = 10.2
+ num2
+ 
+ #pergunta se a variável é inteira
+s= is.integer(num)
+class(s)
+mode(s) 
+typeof(s) 
+
+#converter para inteiro
+
+as.integer("3.70")
+as.integer("joe")
+as.integer('joe')
+as.integer(TRUE)
+as.integer(FALSE)
+as.integer('FALSE')
+
+char1= 'A1'
+char1
+mode(char1)
+typeof(char1)
+
+#complexo
+complexo = 2.5+4i
+complexo
+
+typeof(complexo)
+sqrt(1)
+x=1; y=2
+sqrt(-1-01)
+z= x + y
+z
+class(z) 
+
+u = TRUE ; R =FALSE;
+u&R
+u|R
+!u
+
+6/0
+0/5
+'Joe'/5

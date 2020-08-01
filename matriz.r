2f58361dccb6f1b4166793be50699a8182f9787b
natanguedes <ngsneto@gmail.com>
2020-08-01 19:50:16 -0300

    add

--- matriz.r
+++ matriz.r
@@ -1,27 +1,77 @@
+
+
 getwd()
 
 
-#numero de linhas
-matrix (c(1.2,3,4,5), nr = 2)
-matrix (c(1.2,3,4,5), nr = 4)
-matrix (c(1.2,3,4,5,6), nr = 6)
 
-#matriz de coluna
-matrix (c(1,2,3,4,5,6), nc=3)
-#matrizes precisam ter um numero de  elemento que seja multiplo do numero de linhas
-matrix (c(1,2,3,4,5), nc= 6)
-
-#matrix de linhas
-matrix (c(1,2,3,4,5,6), nr = 3)
+matrix(c(1,2,3,4,5,6) , nr=2)
+matrix(c(1,2,3,4,5,6) , nr=3)
+matrix(c(1,2,3,4,5,6) , nr=5)
 
 
-dados = c(1:30)
-matrix(data = dados, nrow = 5, ncol= 2, byrow =T)
-matrix(data = dados, nrow = 5 ,ncol = 2 )
+meusdados = c(1:10)
+matrix(data = meusdados, nrow = 5, ncol = 2, byrow = T) 
+matrix(data = meusdados, nrow = 5, ncol = 5, byrow = T) 
+
+#Fatiando matrix
+
+m = matrix(c(2,3,4,5), nr =  2)
+m
+
+#matrix transposta
+t(m)
+
+#matriz inversa
+
+solve(m)
+ 
 
 
-#fatiando matrix 
+#multiplicação de matriz
 
-mat =   matrix(c(2,3,4,5), nr = 2)
+
+m = matrix(c(2,3,4,5), nr= 2)
+m
+
+m2 = matrix(c(6,7,8,9), nr = 2)
+m2
+
+m = m+m2
+m
+m-m2
+m/m2
+
+
+#multiplicando matriz de um vetor
+
+x = c(1:4)
+x
+
+y = matrix(c(1,2,3,4), nr = 2)
+x*y
+
+#nomeando matriz
+
+mat = matrix(c("Terra","marte","saturno","Neturno"), nr = 2)
 mat
-mat[1,2] #parou em 05:13 Matrizes, Operações com Matrizes e Matrizes Nomeados - Parte 1/2 08:57
+
+dimnames(mat) = (list(c("linha1","linha2"),c("linha3","linha4")))
+mat
+
+#combinando matrizes
+mat2 = matrix(c(2,4,6,7), nr = 2)
+mat2
+
+mat5 = matrix(c(7,7,9,8), nr = 2)
+mat5
+
+#ligação de linhas
+rbind(mat2,mat5) 
+c(mat5)
+
+cbind(mat2,mat5)
+
+#desconstruindo matriz
+
+c(mat5)
+

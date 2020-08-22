80a4ab391c24ca1d83aa1254f7a7aedcaf39739a
natanguedes <ngsneto@gmail.com>
2020-08-22 11:32:24 -0300

    add

---
+++ Exercicios-Cap02.R
@@ -0,0 +1,50 @@
+# Lista de Exercícios - Capítulo 2
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap02")
+getwd()
+
+# Exercício 1 - Crie um vetor com 30 números inteiros
+
+vetor = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)
+length(vetor)
+vetor.as
+# Exercício 2 - Crie uma matriz com 4 linhas e 4 colunas preenchida com números inteiros
+
+matrix = matrix(1:4,nrow = 4, ncol = 4)
+matrix
+
+# Exercício 3 - Crie uma lista unindo o vetor e matriz criados anteriormente
+
+m = list(matrix)
+m
+
+unir =list(m,vetor)
+unir 
+
+# Exercício 4 - Usando a função read.table() leia o arquivo do link abaixo para uma dataframe
+# http://data.princeton.edu/wws509/datasets/effort.dat
+
+# Exercício 5 - Transforme o dataframe anterior, em um dataframe nomeado com os seguintes labels:
+# c("config", "esfc", "chang")
+
+# Exercício 6 - Imprima na tela o dataframe iris, verifique quantas dimensões existem no dataframe iris e imprima um resumo do dataset
+
+# Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
+
+# Exercício 8 - Usando a função subset, crie um novo dataframe com o conjunto de dados do dataframe iris em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
+
+# Exercícios 9 (Desafio) - Crie um dataframe que seja cópia do dataframe iris e usando a função slice(), divida o dataframe em um subset de 15 linhas
+# Dica 1: Você vai ter que instalar e carregar o pacote dplyr
+# Dica 2: Consulte o help para aprender como usar a função slice()
+
+# Exercícios 10 - Use a função filter no seu novo dataframe criado no item anterior e retorne apenas valores em que Sepal.Length > 6
+# Dica: Use o RSiteSearch para aprender como usar a função filter
+
+

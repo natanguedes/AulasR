c90ff2f06bab1502ec7f46d7003201a762decd44
natanguedes <ngsneto@gmail.com>
2020-08-23 10:25:56 -0300

    add

--- Exercicios-Cap02.R
+++ Exercicios-Cap02.R
@@ -51,16 +51,33 @@ x=nrow(iris)
 #quantidade de colunas
 y=ncol(iris)
 # Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
-  pie(x)
-plot(x,y)
-# Exercício 8 - Usando a função subset, crie um novo dataframe com o conjunto de dados do dataframe iris em que Sepal.Length > 7
-# Dica: consulte o help para aprender como usar a função subset()
 
+    require(stats)
+    pie(table(iris))   
+    graphics.off()
+ 
+# Exercício 8 - Usando a função subset, crie um novo dataframe
+#com o conjunto de dados do dataframe iris em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
+subset(iris,Sepal.Length > 7)
+
+?subset
 # Exercícios 9 (Desafio) - Crie um dataframe que seja cópia do dataframe iris e usando a função slice(), divida o dataframe em um subset de 15 linhas
 # Dica 1: Você vai ter que instalar e carregar o pacote dplyr
 # Dica 2: Consulte o help para aprender como usar a função slice()
+install.packages("dplyr")
+library("dplyr")
+
+?slice
+
+ COPY<-data.frame(iris)
+COPY
+
+slice(COPY, n())
 
 # Exercícios 10 - Use a função filter no seu novo dataframe criado no item anterior e retorne apenas valores em que Sepal.Length > 6
 # Dica: Use o RSiteSearch para aprender como usar a função filter
 
+?filter
 
+filter(COPY,Sepal.Length>7)

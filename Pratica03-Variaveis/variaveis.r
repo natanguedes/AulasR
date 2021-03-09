083c52915aab1813d437b8d31ce9e2555a9c7c5a
ngconsultoriadedados <ngsneto@gmail.com>
2021-03-09 09:45:43 -0300

    Add files via upload

---
+++ Pratica03-Variaveis/variaveis.r
@@ -0,0 +1,28 @@
+
+#variável qualitativas
+
+#ordinal
+
+
+
+meses = c("janeiro","fervereiro","março","ABRIL","MAIO","JUNHO","JULHO","AGOSTO","SETEMBRO","OUTUBRO","NOVEMBRO","DEZEMBRO")
+meses
+
+
+#nominal
+
+animaIS = c("cavalo","cachorro","bezerro")
+animaIS
+
+#quantitativa
+
+#continua
+
+altura = c(1.25,2.35,5.34)
+altura
+
+#discreta
+
+#numero de filhos
+filhos = c(1,3,5,7)
+filhos

1cbf28dbf082c86a765a6fbe750d1a80198eecdc
natanael <ngsneto@gmail.com>
2021-03-23 12:22:24 -0300

    add

--- estruturadecontrole.r
+++ estruturadecontrole.r
@@ -1,50 +1,34 @@
 
 
 getwd()
-# vetores de condição lógica
-cria_x <- TRUE
-cria_y <- FALSE
 
-# só executa se cria_x = TRUE
-if (cria_x) {
-  x <- 1
+x= 35
+if(x<30){
+{"Esse numero é menor que 30"}  
+}else
+{"Este numero é maior que 30" }
+
+x = 7
+if(x<7){
+  "este numero menor que 7"
+} else if(x==7){
+  "x é igual a 7"
+}else {
+  "este numero não é menor que 7"
 }
 
-# só executa se cria_y = TRUE
-if (cria_y) {
-  y <- 1
-}
+x= 5
+if(x==5){
+  "beleza passou"
+}else if(x>10){
+  {"é maior que 10"}
+} else
+{"nao é nenhuma das alternativas acima"}
 
-# note que x foi criado
-exists("x")
-## [1] TRUE
+#ifalse 
+x=5
+ifelse(x < 6 ,"Correto")
 
-# note que y não foi criado
-exists("y")
-## [1] FALSE
-
-
-h = 25
-if(h<-25 && h > 10)
-exists("h")
-
-numero <- 1
-
-if (numero == 6) {
-  cat("o numero é igual a 1")
-} else {
-  cat("o numero não é igual a 1")
-}
-## o n
-
-imparoupar=function(x){
-  if(x %% 2 ==0){
-     return("par")
-} else{
-  return("impar")
-}
-}
-imparoupar(4)
-imparoupar(5)
-x=6
-ifelse(x>5, "maior que 5", NA)
+x=4
+if(x <= 8, "Falso")
+  

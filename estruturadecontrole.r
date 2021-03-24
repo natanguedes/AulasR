837f1832c648decec2c6a78182860d5d4d5efe0c
natanael <ngsneto@gmail.com>
2021-03-24 12:07:22 -0300

    ok

--- estruturadecontrole.r
+++ estruturadecontrole.r
@@ -30,5 +30,81 @@ x=5
 ifelse(x < 6 ,"Correto")
 
 x=4
-if(x <= 8, "Falso")
+if(x < 8){"é verdade" } else {"falso"}
+num = 5
+if(num>5) {"é maior que 5"} else  {"é menor 5 "}
+
+
+y= 5
+ifelse( x<6,"Correto",NA)
+
+x =7
+if( x<7) {"eh menor que 7"}else if(x>7)
+{"eh maior que 7"} else
+{"nenhum dos dois"}
+
+#expressoes ifelse alinhadas 
+
+
+vetor = c(7,5,4)
+ifelse(vetor<5, " é menor que 5", ifelse(vetor==5, "é igual a 5","é maior que 5"))
+
+func = function(x,y){
+  ifelse(y<7, x+y, "Nao encontrado")
   
+}
+
+func(2,2)
+func(40,7)
+func(20,10)
+
+#rep 
+rep(rnorm(10),5)
+
+#repeat
+x = 1
+repeat{
+  x= x +3
+  if(x>99)
+    break
+  print(x)
+}
+
+
+#loop for
+for (i in 1:20) {
+  print(i)
+  
+}
+for(q in rnorm(10)){print(q)}
+#loop for
+for(i in data.frame(10:70)){print(i)}
+for(q in data.frame(rnorm(12))) {print (q)}
+
+#ignora elemento dentro do loop
+for(i in 1:22){
+  if(i ==23){
+    if(i==13 | i ==15)
+      next;
+    print(i)
+  }
+  
+  
+  for(i in 1:22){
+    if(i ==13 | i==15)
+      break;
+    print(i)}
+  
+  #loop while
+  x = 1
+  while(x<5){
+    x=x+1;
+    print(x)
+  }
+}
+
+y= 6
+while(y<5) {
+ x = y+1
+  print(x)
+}

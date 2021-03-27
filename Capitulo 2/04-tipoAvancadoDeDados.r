81e017f59b13888915c81188a9e2dfc3b6804992
natanael <ngsneto@gmail.com>
2021-03-27 12:05:29 -0300

    add

--- Capitulo 2/04-tipoAvancadoDeDados.r
+++ Capitulo 2/04-tipoAvancadoDeDados.r
@@ -25,10 +25,58 @@ class(array)
 typeof(array)
 
 #data.frame utiliza mais de duas dimensões
-#, ou seja, é uma matriz com tipos de dados diferente
+#, ou seja, é uma matriz diferentes tipos de dados 
 
 View(iris)
 
+length(iris)
+mode(iris)
+class(iris)
+typeof(iris)
 
+#listas
+
+# coleção de diferentes tipos de objeto
+# diferentes tipos de dados são possíveis e comuns
+
+lista = list(a= matriz1, b=vetor)
+lista
+
+length(lista)
+mode(lista)
+class(lista)
+typeof(lista)
+
+?list
+
+#funcao também sao vista como objeto em R
+func2 = function(x){
+  var = x + x
+  return(var)   
+}
+func2(2)
+class(func2)
+
+
+#lista os objeto
+objects()
+
+#  [1] "array"      "char1"      "char2"     
+# [4] "compl"      "func2"      "lista"     
+# [7] "matriz1"    "nome"       "num1"      
+# [10] "num2"       "retornaABC" "texto"     
+# [13] "u"          "v"          "valor"     
+# [16] "var"        "var3"       "var4"      
+# [19] "var5"       "var6"       "var8"      
+# [22] "vetor"      "x"          "y"         
+# [25] "z"   
+rm(z) #remove o objeto z 
+z #tenta imprimir o valor de z no console
+#aparece a mensagem no console objeto z não
+vetor
+rm(z,vetor)
+z
+vetor
+objects()
 
 

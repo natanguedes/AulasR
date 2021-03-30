ced1232cc25fd329f5f7c75162f9e30c12fd839d
natanael <ngsneto@gmail.com>
2021-03-30 12:58:28 -0300

    add

---
+++ Cap02/diretory.r
@@ -0,0 +1,21 @@
+
+getwd()
+?setwd()
+
+#nome dos contribuidores
+
+contributors()
+
+
+#licença
+licence()
+
+#informação sobre a sessão
+sessionInfo()
+
+#imprimir na tela
+
+print("testando print...")
+
+#cria grafico 
+plot(1:30)
---
+++ Cap02/pacotes.r
@@ -0,0 +1,12 @@
+
+install.packages("randomForest")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("devtools")
+
+#carrega pacote
+library(ggplot2)
+
+#descarrega pacote
+
+detach(package:ggplot2)

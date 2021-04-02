8ad89616cbb8a095257aeda359341ad5ef43a5c2
natanael <ngsneto@gmail.com>
2021-04-02 13:21:50 -0300

    add

---
+++ Cap02/dataframe.r
@@ -0,0 +1,37 @@
+
+
+#criar dataframe vazio
+
+df = data.frame()
+df
+class(df)
+df
+
+#criando vetores vazios
+
+nome = character()
+idades = numeric()
+itens = numeric()
+codigo = integer()
+
+df = data.frame(c(nome,idades,itens,codigo))
+df
+
+#criando vetores
+
+pais = c("Portugal","Espanha","Itália","Brasil")
+pais 
+
+nome = c("Bruno","Thiago","Amanda","Bianca","Marta")
+nome
+
+altura = c(1.32,2.68,3.87,5.66)
+altura
+
+codigo = c(1001,1002,1003,1004)
+codigo
+
+#dataframe com vetores
+()
+data.frame(c(nome,altura))
+3

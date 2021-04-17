d38a4790e7dd1baaed03f401a33815bf7a3d908d
natanael <ngsneto@gmail.com>
2021-04-17 18:40:23 -0300

    add

--- CAP03/02-tiposdevariaveis.r
+++ CAP03/02-tiposdevariaveis.r
@@ -22,15 +22,47 @@
 
 #$variavel discreta numero inteiro
 set.seed(14)                # Ajustar semente aleatória
-dados <- rchisq(200, 10)    # Gerar 200 observações
-hist(dados) 
+idades <- rchisq(200, 10)    # Gerar 200 observações
+hist(idades) 
 
 #retirado de http://www.leg.ufpr.br/~walmes/ensino/dsbd/7-vis-graphics.html#6_gr%C3%A1fico_para_duas_vari%C3%A1veis_cont%C3%ADnuas
+ 
 
-# Diagrama de dispersão. aa
-url <- "http://www.leg.ufpr.br/~walmes/data/duster_venda_260314.txt"
-dus <- read.table(file = url,
-                  header = TRUE,
-                  sep = "\t",
-                  encoding = "utf-8")
-str(dus)
+
+#quantitativa continua
+altura = c(1.3, 1.33, 1.4, 1.42, 1.45, 1.5, 1.5, 1.52, 1.55, 1.56, 1.58, 1.6, 1.62, 1.64, 1.65, 1.67, 1.68, 1.68, 1.7, 1.72, 1.72, 1.74, 1.8, 1.83, 1.85)
+altura
+hist(altura,  
+     main = "Altura dos alunos do 1º ano do Ensino Médio", 
+     xlab = "Altura dos alunos", ylab = "Freq. Absoluta", 
+     col = c("violet"), 
+     border = FALSE)
+
+ 
+
+#Qualitativa nominal         versao do R 3.6.3
+ 
+library(ggplot2)
+# Barplot
+df <- data.frame(
+        group = c("Male", "Female", "Child"),
+        value = c(25, 25, 50)
+)
+head(df)
+bp<- ggplot(df, aes(x="", y=value, fill=group))+
+        geom_bar(width = 1, stat = "identity")
+bp
+
+pie <- bp + coord_polar("y", start=0)
+pie
+
+
+
+
+
+  grades<-c("Graduação","Pós-graduação-","mestrado","doutorado","pós-doutorado")
+ Marks<-sample(grades,40,replace=T,prob=c(.2,.3,.25,.15,.1))
+ Marks
+barplot(table(Marks),names.arg=grades,col = c("lightblue", 
+                                                      "lightcyan", "lavender", "mistyrose",  "cornsilk"),
+                main="Variavel categórica")

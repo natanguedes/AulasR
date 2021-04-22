0c045e5d82cdcf5b624cc0941519c3733ac0f9d1
natanael <ngsneto@gmail.com>
2021-04-21 23:34:42 -0300

    add

---
+++ CAP03/funcoes.r
@@ -0,0 +1,32 @@
+#objeto no R
+#<- 
+  
+  ?sample
+args(sample)
+args(mean)
+args(sd)
+
+
+#funçoes build-in
+abs(-43)
+sum(c(1,5))
+mean(c(1:5))
+round(c(1.1:5.8))
+rev(c(1:5))
+
+sort(rev(c(1:5)))
+
+append(c(1:5), 6)
+
+v1 = c(1.5,7,8.1,3.7,6.3)
+v2 = rev(v1)
+v2
+
+#funçoes dentro de funçoes
+
+plot(rnorm(10))
+mean(c(abs(v1), abs(v2)))
+
+
+#CRIANDO FUNCOES 04:00 TRABALHANDO COM FUNÇÕES EM R PARTE 1/2
+     

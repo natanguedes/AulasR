2590712fcb3c2b1db0c48489a43ec46c8f21ee5b
natanael <ngsneto@gmail.com>
2021-04-23 22:40:39 -0300

    add

---
+++ CAP03/package.r
@@ -0,0 +1,20 @@
+
+getwd()
+
+#buscando os pacotes
+search()
+install.packages("ggvis")
+install.packages("tm")
+install.packages("dplyr")
+library(tm)
+library(ggvis)
+library(dplyr)
+?require
+install.packages(c("ggplot2","tm","dplyr"))
+#descarregar pacote
+
+detach(package:dplyr)
+
+require(dplyr)
+
+

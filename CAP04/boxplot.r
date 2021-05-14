1c1eb7c8043566e34308164627ca7435f9adfb74
natanael <ngsneto@gmail.com>
2021-05-14 18:26:26 -0300

    add

--- CAP04/boxplot.r
+++ CAP04/boxplot.r
@@ -5,11 +5,18 @@
 attach(sleep)
 
 #construindo boxplot
-
-plot <- boxplot(data = sleep, extra ~ group, main="duração de sono", col.main="red", ylab =  "Horas", xlab = 'Droga')
+                           #~ relação entre as duas variáveis
+plot <- boxplot(data = sleep, extra ~ group, main="duração de sono", col.main="red", ylab =  "Horas", xlab = 'Droga', horizontal =  T)
 
 #cálculo da média
 medias <- by(extra,group,mean)
 medias
 
 points(medias, col = "red")
+
+#Boxplot
+dev.off()
+boxplot(data= sleep, extra~group, ylab= "", xlab="", horizontal =  T)
+horizontalboxplots
+
+boxplot(data= sleep, extra~group, ylab= "", xlab="", horizontal =  T, col= c("blue","red"))

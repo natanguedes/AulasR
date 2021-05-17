05294a7afda5b0c6b65f0e0d8559c235f9ca0a2d
natanael <ngsneto@gmail.com>
2021-05-17 13:11:09 -0300

    add

--- CAP04/pie.r
+++ CAP04/pie.r
@@ -29,3 +29,24 @@ pie(values, labels = labels, main = "Distribuição de Especies" , col = rainbow
 install.packages("plotrix")
 library(plotrix)
 pie(fatias, labels, explode=0.05, col =  c("steelblue1","tomato2","tan3"), main = "Distribuição de vendas")
+
+
+install.packages("plotrix")
+library(plotrix)
+
+pieval<-c(2,4,6,8)
+pielabels<-
+  c("We hate\n pies","We oppose\n  pies","We don't\n  care","We just love pies")
+# grab the radial positions of the labels
+lp<-pie3D(pieval,radius=0.9,labels=pielabels,explode=0.1,main="3D PIE OPINIONS")
+# lengthen the last label and move it to the left
+pielabels[4]<-"We cannot survive without our pies"
+lp[4]<-4.8
+
+pieval<-c(2,4,6,8)
+
+# specify some new colors
+pie3D(pieval,labels=paises,main="Distribuição de vendas",explode = 0.05,
+      col=c("steelblue1","tomato2","tan3",main="Distribuição de vendas"))
+
+

45f5dc3670ccff14fdd98a847e0a377a13b1a887
natanael <ngsneto@gmail.com>
2021-05-17 10:18:55 -0300

    add

--- CAP04/boxplot.r
+++ CAP04/boxplot.r
@@ -1,54 +1,30 @@
-?boxplot
-?sleep
-
-#permite usar as colunas sem especificar o nome do dataset
-attach(sleep)
-
-#construindo boxplot
-                           #~ relação entre as duas variáveis
-plot <- boxplot(data = sleep, extra ~ group, main="duração de sono", col.main="red", ylab =  "Horas", xlab = 'Droga', horizontal =  T)
-
-#cálculo da média
-medias <- by(extra,group,mean)
-medias
-
-points(medias, col = "red")
-
-#Boxplot
-dev.off()
-boxplot(data= sleep, extra~group, ylab= "", xlab="", horizontal =  T)
-horizontalboxplots
-
-boxplot(data= sleep, extra~group, ylab= "", xlab="", horizontal =  T, col= c("blue","red"))
-?cars
-
-View(cars)
 
 getwd()
 
-#construindo um histograma
-?hist
-hist(dados)
-#extraindo colunas
-dados = cars$speed
-dados
-#conforme coñsta no help , o parâmetro breaks pode ser um dos tipos abaixo:
-#um vetor para os pontos de quebra entre as células do histograma
-#uma função para calcular o vetor breakpoints
-#um único número que representa o número de células para o histograma
-#uma cadeia de caracteres que nomeia um algoritmo para calcular o numero de células
-#uma função para calcular o número de células
+#numero de carros num estacionamento
+
+cars = matrix(c(1,2,4,6,7,8,9,12),  nr=4, nc=4)
+cars
+
+
+#nomeando linhas e colunas
+colnames(cars) <-c("0","1-1500","151-300")
+cars
+rownames(cars) <- c("jovem","adulto","idoso")
+cars
+
+
+barplot(cars,  beside = T, legend =  c("steelblue1","tan3","seagreen3"))
+
+barplot(cars, col = c("steelblue1","tan3","seagreen3"), legend =  c("steelblue1","tan3","seagreen3"))
+
  
-# Add a Normal Curve (Thanks to Peter Dalgaard)
-x <- mtcars$mpg
-h<-hist(x, breaks=10, col="red", xlab="Miles Per Gallon",
-        main="Histogram with Normal Curve")
-xfit<-seq(min(x),max(x),length=40)
-yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
-yfit <- yfit*diff(h$mids[1:2])*length(x)
-lines(xfit, yfit, col="blue", lwd=2)
 
 
 
-grafico <- hist(dados, breaks =  30, main =  "Histograma")
+barplot(t(cars), beside = T, col= c("steelblue1","tan3","seagreen3", "peachpuff1"))
+legend("topright", pch = 10,col = c("steelblue1","tan3","seagreen3"), legend =  c("steelblue1","tan3","seagreen3") )
+
+barplot(cars, beside = T ,col = c("steelblue1","tan3","seagreen3") )
+legend("topright", pch = 1,col = c("steelblue1","tan3","seagreen3"), legend =  c("steelblue1","tan3","seagreen3") )
 

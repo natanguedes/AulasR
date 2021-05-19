759a0534338e825f7634f9f7557b621d394e6170
natanael <ngsneto@gmail.com>
2021-05-19 12:08:37 -0300

    add

--- CAP04/ggplot2.r
+++ CAP04/ggplot2.r
@@ -24,3 +24,41 @@ g3 <- g2 + geom_point(aes(color = clarity)) + geom_smooth(method = "lm", se = TR
 g3
 
 g3 + xlab("Quilates") + ylab("Preco")
+g3 + xlim(0, 10) + ylim(0, 30000)
+
+
+g3 + xlim(0, 10) + ylim(0, 30000) + annotate("text", x = 9.5, y = 30000, label = "Painel a", color = "red")
+
+g3 + xlim(0, 10) + ylim(0, 30000) +
+  annotate("rect", xmin = 3, xmax = 4, ymin = 10000,
+           ymax = 20000, alpha = .2, color = "blue", fill = "green")
+
+g3 + xlim(0, 10) + ylim(0, 30000) +
+  annotate("segment", x = 3, xend = 4, y = 30000, yend = 20000,
+           color = "orange", arrow = arrow(length = unit(1, "cm")))
+
+g2 + geom_point(aes(color = clarity)) + scale_color_brewer(type = "seq", palette = "Set3")
+g2 + geom_point(aes(color = clarity)) + facet_grid(. ~ cut)
+
+g3 + xlim(0, 10) + ylim(0, 30000) + annotate("text", x = 9.5, y = 30000, label = "Painel a", color = "red")
+
+g3 + xlim(0, 10) + ylim(0, 30000) +
+  annotate("rect", xmin = 3, xmax = 4, ymin = 10000,
+           ymax = 20000, alpha = .2, color = "blue", fill = "green")
+
+g3 + xlim(0, 10) + ylim(0, 30000) +
+  annotate("segment", x = 3, xend = 4, y = 30000, yend = 20000,
+           color = "orange", arrow = arrow(length = unit(1, "cm")))
+
+g2 + geom_point(aes(color = clarity)) + scale_color_brewer(type = "seq", palette = "Set3")
+g2 + geom_point(aes(color = clarity)) + facet_grid(. ~ cut)
+g2 + geom_point(aes(color = clarity)) + facet_grid(cut ~ .)
+g2 + geom_point(aes(color = clarity)) + facet_grid(cut ~ clarity)
+g3 + xlim(0, 10) + ylim(0, 30000) + theme(legend.position = "none")
+g3 + xlim(0, 10) + ylim(0, 30000) + theme(legend.position = "bottom")
+g3 + xlim(0, 10) + ylim(0, 30000) + theme(legend.position = "bottom", legend.title = element_blank())
+g3 + xlim(0, 10) + ylim(0, 30000) + theme(legend.position = c(.8,.8))
+
+g3 + xlim(0, 10) + ylim(0, 30000) + theme(legend.position = c(.8,.2))
+g3 + theme_bw()
+g3 + theme(panel.grid.major = element_line(colour = "red", linetype = "dotted"), panel.background = element_rect(fill = "green"))

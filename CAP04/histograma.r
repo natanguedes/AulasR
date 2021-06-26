e20eb198465d68e789fdda86e740e1e3944c288c
natanael <ngsneto@gmail.com>
2021-06-26 17:53:20 -0300

    add

--- CAP04/histograma.r
+++ CAP04/histograma.r
@@ -1,24 +1,43 @@
+
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap04")
+getwd()
+
+# Definindo os dados
 ?cars
 View(cars)
-dados= cars$speed
-dados
+dados = cars$speed
 
-#construindo um histograma
+# Construindo um histograma
 ?hist
 hist(dados)
-hist(dados, col=rainbow(20))
 
-hist(dados, breaks = 10, col = rainbow(30), main = "Histograma das velocidades")
-hist(dados, labels = T, col=rainbow(30), breaks = c(0.5,10,20,30), main = "Histogramas das velocidades")
-hist(dados, labels = T, ylim = c(0,10), breaks = 10, col = rainbow(30), main = "Histograma de velocidades")
 
-grafico <- hist(dados, breaks = 30, main = "Histograma das velocidades")
+# Conforme consta no help, o parâmetro breaks pode ser um dos itens abaixo:
+# Um vetor para os pontos de quebra entre as células do histograma
+# Uma função para calcular o vetor de breakpoints
+# Um único número que representa o número de células para o histograma
+# Uma cadeia de caracteres que nomeia um algoritmo para calcular o número de células 
+# Uma função para calcular o número de células.
+hist(dados, breaks = 10, main = "Histograma das Velocidades")
+hist(dados, labels = T, breaks = c(0,5,10,20,30), main = "Histograma das Velocidades")
+hist(dados, labels = T, breaks = 10, main = "Histograma das Velocidades")
+hist(dados, labels = T, ylim = c(0,10), breaks = 10, main = "Histograma das Velocidades")
 
-xaxis = seq(min(dados), max(dados), length = 30)
+
+# Adicionando linhas ao histograma
+grafico <- hist(dados, breaks = 10, main = "Histograma das Velocidades")
+
+xaxis = seq(min(dados), max(dados), length = 10)
 yaxis = dnorm(xaxis, mean = mean(dados), sd = sd(dados))
 yaxis = yaxis*diff(grafico$mids)*length(dados)
 
-lines(xaxis, yaxis, col="red")
+lines(xaxis, yaxis, col = "red")
 
 
-                            

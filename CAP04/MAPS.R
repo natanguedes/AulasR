bf44536cf31fa5a8e49fc0f8b3acffb873d7449a
natanael <ngsneto@gmail.com>
2021-07-08 10:56:11 -0300

    add

--- CAP04/MAPS.R
+++ CAP04/MAPS.R
@@ -21,3 +21,29 @@ ggplot()+geom_polygon(data = mapa, aes(x=long,y=lat, group=group ),fill=NA, colo
 g1=ggplot()+geom_polygon(data = mapa, aes(x=long,y=lat, group=group ),fill="seagreen1", color="blue")+coord_fixed()
 g1
 #marcando pontos no mapa
+
+# Marcando alguns pontos no mapa
+# Podemos usar um shapefile
+labs <- data.frame(
+  long = c(69.24140, -2.8456051),
+  lat = c(-78.38995, 22.44512),
+  names = c("Ponto1", "Ponto2"),
+  stringsAsFactors = FALSE
+)  
+
+# Pontos no mapa
+gg1 + 
+  geom_point(data = labs, aes(x = long, y = lat), color = "black", size = 2) +
+  geom_point(data = labs, aes(x = long, y = lat), color = "yellow", size = 2)
+
+# Divisão por países
+ggplot(data = mapa) + 
+  geom_polygon(aes(x = long, y = lat, fill = region, group = group), color = "white") + 
+  coord_fixed(1.3) +
+  guides(fill=FALSE)
+
+
+# rMaps 
+# http://rmaps.github.io
+
+
